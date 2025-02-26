912f4e30c3b37a2bd0aec3a3936a221c4a4909a1
NoemiPast <lic.noemipastrana@gmail.com>
2025-02-25 21:39:00 -0600

    Add files via upload
    OPERADOR LÓGICO Y
    Llevado a cabo en un ejemplo de validación de credencial.
    Contiene comentarios que ayudan a entender los comandos que se emplearon.

---
+++ 1.OperadorLógico.py
@@ -0,0 +1,34 @@
+#def es para definir una función, la cual puede ser usada después
+def validar_credenciales(usuario, contraseña):
+    # Verifica que el usuario tenga al menos 5 caracteres
+    condicion_usuario = len(usuario) >= 5
+    
+    # Verifica que la contraseña tenga al menos 8 caracteres y contenga al menos un número
+    #"for" e "in" es un bucle que recorre la cadena "contraseña", caracter por caracter
+    #"any" es una función que toma un iterable (es lo que está entre paréntesis) y devuelve "true" 
+    #si hay al menos un número en la contraseña. Y si no hay ningún número devuelve "false"
+    condicion_contraseña = len(contraseña) >= 8 and any(caracter.isdigit() for caracter in contraseña)
+    #isdigit es un método incorporado en Python que se usa en cadenas de texto PARA VERIFICAR SI EL 
+    #CARACTER QUE SE INVOCA ES UN DÍGITO NUMÉRICO
+
+    # Si ambas condiciones son verdaderas, las credenciales son válidas
+
+    if condicion_usuario and condicion_contraseña:
+        return True
+    else:
+        return False
+
+# Ejemplo de uso
+usuario = "usuario123"
+contraseña = "clave1234"
+
+print ("Ingrese el usuario")
+usuario = (input("usuario:"))
+
+print ("Ingrese su contraseña")
+contraseña = (input("contraseña:"))
+
+if validar_credenciales(usuario, contraseña):
+    print("Credenciales válidas. Inicio de sesión exitoso.")
+else:
+    print("Credenciales inválidas. Por favor, verifica tu usuario y contraseña.")
